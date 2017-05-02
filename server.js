f1934bbf3dbc51973499ce3e47c0abd139164dbe
Taiwo <andeladeveloper@Andelas-MacBook-Pro.local>
2017-05-02 11:01:17 +0100

    add morgan for logging requests and passport for authentication

--- server.js
+++ server.js
@@ -4,17 +4,20 @@ const documentRouter = require('./server/routes/documentRoutes.js');
 const searchRouter = require('./server/routes/searchRoutes.js');
 const roleRouter = require('./server/routes/roleRoutes.js');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const passport = require('./server/middlewares/authentication');
 
 const app = express();
 const port = 3000;
 
-app.listen(port, (error) => {
+app.listen(port, () => {
   console.log(`running server on port ${port}`);
 });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(morgan('dev'));
+app.use(passport.initialize());
 app.use(express.static('client/views'));
 app.use('/users', userRouter);
 app.use('/documents/', documentRouter);
