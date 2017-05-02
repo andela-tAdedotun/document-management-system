b3d620ea48a7342d8787b358c07aab06d40a1081
Taiwo <andeladeveloper@Andelas-MacBook-Pro.local>
2017-05-02 10:03:38 +0100

    add authorization for document search super admin can access all documents an admin has access to their documents, public and 'role' documents of other admins and documents belonging to regular users a regular user can only access their own documents in addition to public and 'role' documents of other regular users

--- server/controllers/searchController.js
+++ server/controllers/searchController.js
@@ -40,8 +40,31 @@ module.exports = {
   */
   searchDocuments(req, res) {
     const searchQuery = req.query.search;
-    const searchOptions = {
-      where: {
+    const searchOptions = {};
+
+    // Default value for a super admin. Can access all documents.
+    searchOptions.where = {
+      $or: [
+        {
+          title: {
+            $iLike: `%${searchQuery}%`,
+          }
+        },
+        {
+          content: {
+            $iLike: `%${searchQuery}%`,
+          }
+        }
+      ]
+    };
+    /*
+      Document access for admins.
+      An admin can access all public documents, private and 'role' documents
+      belonging to users with less privilege, and 'role' documents belonging
+      to admins in addition to the admin's documents.
+    */
+    if (req.user.roleId === 2) {
+      searchOptions.where = {
         $or: [
           {
             title: {
@@ -53,9 +76,78 @@ module.exports = {
               $iLike: `%${searchQuery}%`,
             }
           }
-        ]
-      }
-    };
+        ],
+        $and: {
+          $or: [
+            { access: 'public' },
+            { access: 'private',
+              $not: {
+                '$User.RoleId$': 1
+              },
+              $or: {
+                '$User.id$': req.user.id,
+                '$User.RoleId$': { $gt: 2 }
+              }
+            },
+            { access: 'role',
+              $not: {
+                '$User.RoleId$': 1
+              }
+            },
+          ]
+        }
+      };
+
+      searchOptions.include = [
+        {
+          model: User,
+          attributes: { exclude: ['password', 'privacy', 'RoleId'] }
+        }
+      ];
+      /*
+        Document access for regular users.
+        Regular users can only access another user's documents if those
+        documents are public or 'role'. They can access all their own
+        documents
+      */
+    } else if (req.user.roleId > 2) {
+      searchOptions.where = {
+        $or: [
+          {
+            title: {
+              $iLike: `%${searchQuery}%`,
+            }
+          },
+          {
+            content: {
+              $iLike: `%${searchQuery}%`,
+            }
+          }
+        ],
+        $and: {
+          $or: [
+            { access: 'public' },
+            { access: 'role',
+              $and: {
+                '$User.RoleId$': req.user.roleId
+              }
+            },
+            { access: 'private',
+              $and: {
+                documentOwnerId: req.user.id
+              }
+            }
+          ]
+        }
+      };
+
+      searchOptions.include = [
+        {
+          model: User,
+          attributes: { exclude: ['password', 'privacy', 'RoleId'] }
+        }
+      ];
+    }
 
     Document.findAll(searchOptions)
       .then(documentMatches => res.status(200).send(documentMatches))
