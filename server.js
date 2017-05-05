0be45708cd12ea00d3245c3a62b7fb47c2c93a77
Taiwo <andeladeveloper@Andelas-MacBook-Pro.local>
2017-05-05 22:35:52 +0100

    add ES6 style import/export. implement Webpack in server file

--- server.js
+++ server.js
@@ -1,29 +1,42 @@
-const express = require('express');
-const userRouter = require('./server/routes/userRoutes.js');
-const documentRouter = require('./server/routes/documentRoutes.js');
-const searchRouter = require('./server/routes/searchRoutes.js');
-const roleRouter = require('./server/routes/roleRoutes.js');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const passport = require('./server/middlewares/authentication');
+/* eslint-disable no-console */
+import webpack from 'webpack';
+import WebpackHotMiddleware from 'webpack-hot-middleware';
+import WebpackDevMiddleware from 'webpack-dev-middleware';
+import express from 'express';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import userRouter from './server/routes/UserRoutes';
+import documentRouter from './server/routes/DocumentRoutes';
+import searchRouter from './server/routes/SearchRoutes';
+import roleRouter from './server/routes/RoleRoutes';
+import config from './webpack.config.dev';
+import passport from './server/middlewares/Authentication';
+
+const compiler = webpack(config);
 
 const app = express();
 const port = 3000;
-
-app.listen(port, () => {
-  console.log(`running server on port ${port}`);
-});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan('dev'));
 app.use(passport.initialize());
-app.use(express.static('client/views'));
+app.use(WebpackDevMiddleware(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
+app.use(WebpackHotMiddleware(compiler));
+
 app.use('/users', userRouter);
 app.use('/documents/', documentRouter);
 app.use('/search', searchRouter);
 app.use('/roles', roleRouter);
 
-app.get('/', (req, res) => {
-  res.send('Hello world!');
+app.get('*', (req, res) => {
+  res.sendFile(`${__dirname}/client/index.html`);
+});
+
+// app.use(express.static('./client/index.html'));
+app.listen(port, () => {
+  console.log(`running server on port ${port}`);
 });
