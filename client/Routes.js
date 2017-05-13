afff3068e04ced54d52984935b552ab555e35639
Taiwo <andeladeveloper@Andelas-MacBook-Pro.local>
2017-05-13 10:17:35 +0100

    user is redirected to home from landing page if already logged in

--- client/Routes.js
+++ client/Routes.js
@@ -5,10 +5,11 @@ import LandingPage from './components/landing/LandingPage';
 import SignupPage from './components/signup/SignupPage';
 import Homepage from './components/home/Homepage';
 import requiresAuthentication from './utilities/RequiresAuthentication';
+import alreadyAuthenticated from './utilities/AlreadyAuthenticated';
 
 export default (
   <Route path="/" component={App}>
-    <IndexRoute component={LandingPage} />
+    <IndexRoute component={alreadyAuthenticated(LandingPage)} />
     <Route path="signup" component={SignupPage} />
     <Route path="documents" component={requiresAuthentication(Homepage)} />
   </Route>
