6ec4227366042ffe716ecd87e5f2d9c6688e42bd
Taiwo <andeladeveloper@Andelas-MacBook-Pro.local>
2017-05-14 19:56:00 +0100

    add component to display user documents to them

--- client/components/home/DisplayUserDocuments.js
+++ client/components/home/DisplayUserDocuments.js
@@ -1,10 +1,60 @@
 import React from 'react';
-
+import { Row, Modal, Input } from 'react-materialize';
 
 /**
  *
  */
 class DisplayUserDocuments extends React.Component {
+  /**
+   * constructor - description
+   *
+   * @param  {type} props description
+   * @return {type}       description
+   */
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: this.props.document.title,
+      content: this.props.document.content,
+      access: this.props.document.access
+    };
+    this.deleteDocument = this.deleteDocument.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  /**
+   * onChange - description
+   *
+   * @param  {type} event description
+   * @return {type}       description
+   */
+  onChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  /**
+   * onSubmit - description
+   *
+   * @return {type}  description
+   */
+  onSubmit(event) {
+    event.preventDefault();
+    this.props.editDocument(this.props.document.id, this.state);
+  }
+
+
+  /**
+   * editDocument - description
+   *
+   * @return {type}  description
+   */
+  deleteDocument() {
+    this.props.deleteDocument(this.props.document.id);
+  }
+
   /**
    * render - description
    *
@@ -13,15 +63,80 @@ class DisplayUserDocuments extends React.Component {
   render() {
     const { title, content } = this.props.document;
     return (
-      <div>
-        {title}: {content.slice(0, 25)}...
+      <div className="row">
+        <div className="col m3">
+          <div className="card small blue-grey darken-1">
+            <div className="card-content white-text">
+              <span className="card-title">{title}</span>
+              {content.slice(0, 200)}...
+            </div>
+            <div className="card-action">
+
+              <Modal
+                fixedFooter
+                trigger={<button className="btn">
+                  Edit
+                </button>}
+              >
+
+                <form onSubmit={this.onSubmit}>
+                  <div>
+                    Title: <br />
+                    <input
+                      name="title"
+                      value={this.state.title}
+                      type="text"
+                      onChange={this.onChange}
+                      required
+                    />
+                  </div>
+                  <div>
+                    Content: <br />
+                    <textarea
+                      name="content"
+                      onChange={this.onChange}
+                      value={this.state.content}
+                      required
+                    />
+                    <br />
+                  </div>
+
+                  <div>
+                    <Row>
+                      <Input
+                        s={12}
+                        type="select"
+                        name="access"
+                        label="Who Can Access"
+                        onChange={this.onChange}
+                      >
+                        <option value="public">Public</option>
+                        <option value="private">Private</option>
+                        <option value="role">Role</option>
+                      </Input>
+                    </Row>
+                  </div>
+                  <button type="submit"> Submit! </button>
+                </form>
+              </Modal>
+              <button className="btn red" onClick={this.deleteDocument}>
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
+      // <div>
+      //   {title}: {content.slice(0, 25)}...
+      // </div>
     );
   }
 }
 
 DisplayUserDocuments.propTypes = {
-  document: React.PropTypes.object.isRequired
+  document: React.PropTypes.object.isRequired,
+  deleteDocument: React.PropTypes.func.isRequired,
+  editDocument: React.PropTypes.func.isRequired
 };
 
 export default DisplayUserDocuments;
