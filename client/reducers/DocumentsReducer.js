946cf0cb536c178ea4a9a84eb8b093ccfc043284
Taiwo <andeladeveloper@Andelas-MacBook-Pro.local>
2017-05-14 19:56:49 +0100

    add reducer to handle document actions handles displaying documents user has access to

--- client/reducers/DocumentsReducer.js
+++ client/reducers/DocumentsReducer.js
@@ -1,19 +1,10 @@
 export default (state = [], action = {}) => {
   switch (action.type) {
-    case 'DISPLAY_USER_DOCUMENTS': {
+    case 'DISPLAY_DOCUMENTS': {
       return [
         ...state,
         {
-          userDocuments: action.userDocuments
-        }
-      ];
-    }
-
-    case 'USER_HAS_NO_DOCUMENT': {
-      return [
-        ...state,
-        {
-          noDocument: action.errorMessage
+          documents: action.documents
         }
       ];
     }
