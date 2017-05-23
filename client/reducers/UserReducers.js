3531fa76e2845864a8bf2bc6f10b3949fc6dc09c
Taiwo <andeladeveloper@Andelas-MacBook-Pro.local>
2017-05-23 23:56:34 +0100

    minor refactoring to reflect new state structure

--- client/reducers/UserReducers.js
+++ client/reducers/UserReducers.js
@@ -8,23 +8,23 @@ export default (state = [], action = {}) => {
 
     case 'ADMIN_UPDATE_USER': {
       const index =
-        findIndex(state[0].allUsers.users, { id: action.userId });
-      const stateCopy = [...state];
-      stateCopy[0].allUsers.users[index] = action.updatedUser;
+        findIndex(state.users, { id: action.userId });
+      const stateCopy = Object.assign({}, state);
+      stateCopy.users[index] = action.updatedUser;
       return stateCopy;
     }
 
     case 'DELETE_USER': {
       const index =
-        findIndex(state[0].allUsers.users, { id: action.userId });
-      const stateCopy = [...state];
-      stateCopy[0].allUsers.users.splice(index, 1);
+        findIndex(state.users, { id: action.userId });
+      const stateCopy = Object.assign({}, state);
+      stateCopy.users.splice(index, 1);
       return stateCopy;
     }
 
     case 'CREATE_NEW_USER': {
-      const stateCopy = [...state];
-      stateCopy[0].allUsers.users.push(action.createdUser);
+      const stateCopy = Object.assign({}, state);
+      stateCopy.users.push(action.createdUser);
       return stateCopy;
     }
 
